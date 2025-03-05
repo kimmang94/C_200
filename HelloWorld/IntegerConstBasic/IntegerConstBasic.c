6cba01404c62c3045d5776bfee37ebc7864039f7
KIMHYEOK\borad <kimmang940715@naver.com>
2025-03-06 01:31:55 +0900

    정수형 상수 개념

---
+++ HelloWorld/IntegerConstBasic/IntegerConstBasic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define  HUNDERED_THOUSAND  1000000
+
+const int j = 2000000;
+main()
+{
+	
+	// HUNDERED_THOUSAND = 100000;
+	// j = 20000;
+
+	printf("정수형 상수 HUNDRED_THOUSAND의 값은 %d \n", HUNDERED_THOUSAND);
+	printf("정수형 상수 j의 값은 %d \n", j);
+}
