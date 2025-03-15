0d6ff4c1c06230f46c6a04a60c384bd80374321b
KIMHYEOK\borad <kimmang940715@naver.com>
2025-03-15 11:03:18 +0900

    분기문

---
+++ HelloWorld/Switch_Case_Default/Switch_Case_Default.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main()
+{
+	int i = 5;
+
+	switch (1)
+	{
+	case 1:
+		printf("i는 1입니다");
+		break;
+	case 2:
+		printf("i는 2입니다");
+		break;
+	case 3:
+		printf("i는 3입니다");
+		break;
+	}
+}
